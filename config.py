bd08b3ed73e7988c330743376dd83d84106b8358
Eiji Dougal <eiji.dougal@gmail.com>
2023-03-09 18:06:21 -0700

    add config for constants and other settings

---
+++ config.py
@@ -0,0 +1,32 @@
+# config file for constants
+
+# colors
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
+PURPLE = (128, 0, 128)
+CYAN = (0, 255, 255)
+PINK = (255, 192, 203)
+BROWN = (165, 42, 42)
+GREY = (128, 128, 128)
+
+# screen size
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# game settings
+FPS = 60
+TITLE = "Tetris"
+BGCOLOR = BLACK
+
+# player settings
+PLAYER_SPEED = 5
+PLAYER_SIZE = 20
+
+# block settings
+BLOCK_SIZE = 20
--- player.py
+++ player.py
@@ -3,9 +3,12 @@ import pygame
 
 # player object for tetris
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, game):
         pygame.sprite.Sprite.__init__(self)
-        pass
+        self.game = game
+        self.image = pygame.Surface((20, 20))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect()
 
     def update(self):
         pass
